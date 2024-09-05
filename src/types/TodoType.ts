9072f8d75b5b956377d42723ec0aed7a985a984b
sk <sungkeno3o@gmail.com>
2024-09-05 15:10:57 +0900

    Update . 투두리스트 아이템 타입 정의 수정

--- src/types/TodoType.ts
+++ src/types/TodoType.ts
@@ -1,7 +1,8 @@
 export interface Todo {
     id : number ; 
     title : string ;
+    created_at : Date | string ;
+    updated_at? : Date | string; 
     tag? : string ; 
-    created_at : Date ;
-    updated_at? : Date ; 
+    isDone : boolean;
 }
