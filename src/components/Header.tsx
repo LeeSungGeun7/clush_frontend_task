2b3dab65a4d043ee7ef8739c65d3b2f5415078a2
sk <sungkeno3o@gmail.com>
2024-09-05 15:07:54 +0900

    Feature . 웹버젼 헤더 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const HeaderStyle = styled.header`
+    display: flex;
+    align-items: center;
+    padding-left: 30px;
+    width : 100vw;
+    height: 70px;
+    background-color: #f1efef;
+    @media (max-width: 400px) {
+        display:none;
+    }
+`
+
+function Header() {
+  return (
+    <HeaderStyle>
+        TODO TASK
+    </HeaderStyle>
+  )
+
+
+
+}
+
+export default Header
