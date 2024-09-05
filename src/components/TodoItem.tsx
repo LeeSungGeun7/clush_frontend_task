974c0c7c44b3211c1992252c7bce44dfe599293b
sk <sungkeno3o@gmail.com>
2024-09-05 15:12:18 +0900

    Feature . 투두리스트 아이템 컴포넌트 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { Todo } from '../types/TodoType'
-
+import styled from 'styled-components'
 
 // 재사용이 많이 되는 주력 컴포넌트 
 // 카드 형식의 UI 
 // 투두리스트에서 생성/수정/삭제 기능을 포함
 
 
+const TodoContainer = styled.div`
+    display:flex;
+    background-color: white;
+    margin:5px;
+    justify-content: space-between;
+    align-items:center;
+    width: 100%;
+    height: 100px;
+
+`
+
 
 function TodoItem({title,created_at,tag,updated_at}:Todo) {
   return (
-    <div>
+    <TodoContainer>
         {title}
-    </div>
+    </TodoContainer>
   )
 }
 
