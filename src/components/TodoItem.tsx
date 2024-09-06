12c177a870438a570edc59f0117ff62905b794a0
sk <sungkeno3o@gmail.com>
2024-09-06 14:22:19 +0900

    Feature . 투두아이템 컴포넌트 기능 스타일 추가 변경

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,28 +1,67 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Todo } from '../types/TodoType'
 import styled from 'styled-components'
+import { Checkbox } from "antd";
+import { CloseCircleOutlined , EditOutlined} from '@ant-design/icons'
+import { useTodo } from '../hooks/useTodo';
 
-// 재사용이 많이 되는 주력 컴포넌트 
+// 재사용이 많이 되는 아이템 컴포넌트 
 // 카드 형식의 UI 
 // 투두리스트에서 생성/수정/삭제 기능을 포함
 
-
-const TodoContainer = styled.div`
+const TodoContainer = styled.div<any>`
     display:flex;
     background-color: white;
     margin:5px;
     justify-content: space-between;
     align-items:center;
     width: 100%;
-    height: 100px;
+    height: 100px;   
 
+    .right {
+        display : flex;
+
+        * {
+            margin:3px;
+
+            *:hover {
+                transition: 1s 'easy-in';
+                font-size:1.5rem;    
+            }
+
+        }
+    }
+
+    span {
+        text-decoration : ${(props)=>props.isdone.toString() === "true" ? 'line-through' : 'none' }
+    }
+    
 `
 
 
-function TodoItem({title,created_at,tag,updated_at}:Todo) {
+
+function TodoItem({title,created_at,tag,updated_at,id,isDone}:Todo) {
+    const { deleteSelectedTodo , setTodoList , todolist , changeSelectedTodo  } = useTodo();
+    
+    const onChangeHandler = () => {
+        const updatedTodo = { id, title, isDone: !isDone, created_at };
+        changeSelectedTodo(id, updatedTodo); // 변경된 값을 직접 전달
+    };
+
   return (
-    <TodoContainer>
-        {title}
+    <TodoContainer key={id} isdone={isDone+""}>
+        <Checkbox checked={isDone} onChange={()=>{onChangeHandler();}} >
+            <span>{title}</span>
+        </Checkbox>
+        
+        <div className='right'>
+        <EditOutlined />
+        <CloseCircleOutlined onClick={()=>{
+            deleteSelectedTodo(id);
+            console.log(id,todolist)
+        }}/>
+        </div>
+        
     </TodoContainer>
   )
 }
